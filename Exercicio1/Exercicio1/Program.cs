a3d3acd75bd51aa9af33c74b38488092a608a2c6
DinHoo <51788868+DinHoo@users.noreply.github.com>
2019-07-04 10:05:12 -0300

    1 já foi

--- Exercicio1/Exercicio1/Program.cs
+++ Exercicio1/Exercicio1/Program.cs
@@ -10,6 +10,24 @@ namespace Exercicio1
     {
         static void Main(string[] args)
         {
+            float[] numeros = new float[5];
+            int count = 0;
+            Console.WriteLine("Digite 5 valores para saber quais são negativos");
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = (float)Convert.ToDouble(Console.ReadLine());
+                if (numeros[i] < 0) count++;
+            }
+
+            Console.WriteLine("Existem " + count + " numeros negativos, são eles:");
+            /* Teste de respostas
+            foreach (float num in numeros)
+            {
+                if (num < 0) Console.WriteLine(num);
+            }
+            */
+            Console.ReadKey();
         }
     }
 }
